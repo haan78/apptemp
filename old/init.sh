212274f830e04da2f025cbf31e0a4c94e6087198
Ali Barış Öztürk <haan78@gmail.com>
2020-07-08 15:37:45 +0300

    db changes

--- old/init.sh
+++ old/init.sh
@@ -1,22 +1,17 @@
 #!/bin/bash
-
-basedir=$(dirname "$0")
-createsql="$basedir/create.sql"
-updatesql="$basedir/update.sql"
-
 echo "Starting to Apache"
 service apache2 start
 
 if [ ! -z "$MYSQL_CONNECTION_STRING" ]
 then
-    echo "--> Parsing MySql connection string..."
+    echo "-> Parsing MySql connection string..."
     host=$(cut -d "," -f 1 <<< $MYSQL_CONNECTION_STRING)
     user=$(cut -d "," -f 2 <<< $MYSQL_CONNECTION_STRING)
     pass=$(cut -d "," -f 3 <<< $MYSQL_CONNECTION_STRING)
     dbname=$(cut -d "," -f 4 <<< $MYSQL_CONNECTION_STRING)
     port=$(cut -d "," -f 5 <<< $MYSQL_CONNECTION_STRING)
 
-    echo "--> Adding connection parameter into php.ini"
+    echo "-> Adding connection parameter into php.ini"
     echo "mysqli.default_host = $host" >> /etc/php/7.4/apache2/conf.d/20-mysqli.ini
     echo "mysqli.default_user = $user" >> /etc/php/7.4/apache2/conf.d/20-mysqli.ini
     echo "mysqli.default_pw = $pass" >> /etc/php/7.4/apache2/conf.d/20-mysqli.ini
@@ -24,7 +19,7 @@ then
 
     while ! mysqladmin ping -h"$host" --silent
     do
-        echo "--> Waiting for MySql Server"
+        echo "-> Waiting for MySql Server"
         sleep 1
     done
 
@@ -35,39 +30,31 @@ then
         mycon="mysql -h $host -u$user -p$pass --port=$port"
     fi
 
-    echo "--> Checking database..."
-    result=$($mycon -e "SHOW DATABASES LIKE '$dbname'")
-    if [[ $result != *"$dbname"* ]]
-    then
-        if [ -f "$createsql" ]
+    echo "-> Checking database scripts..."
+    for sql in /scripts/databases/*.sql
+    do
+        filename=$(basename -- "$sql")
+        dbname="${filename%.*}"
+        echo "File control $sql to $dbname in mysql-server"
+        result=$($mycon -e "SHOW DATABASES LIKE '$dbname'")
+        if [[ $result != *"$dbname"* ]]
         then
-            echo "--> Deoploying Creation Script $createsql ..."
-            $($mycon < $createsql )
-            if [ $? -ne 0 ]
+            echo "-> Deoploying SQL script $sql"
+            $($mycon < $sql )
+            if [ $? -eq 0 ]
             then
+                echo "-> Script $sql executed successfuly"
+            else
                 exit 1
-            fi
+            fi        
         else
-            echo "--> ERROR: $createsql not found!"
-            exit 1
+            echo "-> Database $dbname is already exist"
         fi
-    fi
-
-    if [ -f "$updatesql" ]
-    then
-        echo "--> Deoploying Update Script $updatesql ..."
-        $($mycon < $updatesql )
-        if [ $? -ne 0 ]
-        then
-            exit 1
-        fi
-    else
-        echo "--> $updatesql not found. OK! No Problem"
-    fi
+    done
 else
-    echo "--> Environment Variable MYSQL_CONNECTION_STRING is not found"
+    echo "-> Environment Variable MYSQL_CONNECTION_STRING is not found"
     exit 1
 fi
 
-echo "---> It seems all done. Good luck!!!"
+echo "-->> It seems all done. Good luck!!!"
 exec supervisord -n
---
+++ server/scripts/update.sql
@@ -0,0 +1,6 @@
+USE test1;
+
+CREATE TABLE IF NOT EXISTS `test` ( 
+      `id` int(11) DEFAULT NULL, 
+      `resultTest` varchar(30)
+);
