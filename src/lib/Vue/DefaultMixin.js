495f3ada7bee7df0db8ffaf2e7655709a16a2527
Ali Barış Öztürk <haan78@gmail.com>
2020-07-10 12:00:57 +0300

    ajax

--- src/lib/Vue/DefaultMixin.js
+++ src/lib/Vue/DefaultMixin.js
@@ -1,5 +1,17 @@
 
 export default {
+
+    data() {
+        return {
+            ajaxActivationCount:0
+        }
+    },
+
+    computed:{
+        loading() {
+            return (this.ajaxActivationCount > 0);
+        }
+    },
     
     methods:{
 
@@ -35,16 +47,6 @@ export default {
             }
         },
 
-        setLoading(value) {
-            let self = this;
-            if ( typeof self.$parent.loading === "boolean" ) {
-                self.$parent.loading = value;
-            }
-            if ( typeof self.loading === "boolean" ) {
-                self.loading = value;
-            }
-        },
-
         setData(t,s) {
             if ( typeof t === "object" && t !== null ) {
                 for (var k in t) {
@@ -64,26 +66,48 @@ export default {
                 message: message
               });
         },
+
+        defaultAjaxResponseEvaluation(data) {
+            if ( !data.ERROR ) {
+                return {
+                    "success":true,
+                    "data":data
+                }
+            } else {
+                return {
+                    "success":false,
+                    "text":data.ERROR
+                }
+            }
+        },
         
-        WebMethod(method,data,onSuccess,onError) {
+        ajax(url,data,onSuccess,onError) {
+            
             let self = this;
             var err = ( typeof onError === "function" ? onError : self.defaultError );
-            self.setLoading(true);
-            self.$http.post("index.php/"+method+"?a=ajax",(data ? data : null )).then( (response)=>{
-                self.setLoading(false);
+            self.ajaxActivationCount += 1;
+            self.$http.post(url,(data ? data : null )).then( (response)=>{
+                if (self.ajaxActivationCount>0) {
+                    self.ajaxActivationCount -= 1;
+                } 
                 if ( typeof response.data === "object" ) {
-                    if ( response.data.success ) {
+
+                    var are = self.defaultAjaxResponseEvaluation(response.data);
+                    if (are.success) {
                         if ( typeof onSuccess === "function" ) {
                             onSuccess( response.data );
                         }
                     } else {
-                        err( "Application",response.data.text );
+                        err( "Application",response.data );
                     }
+
                 } else {
                     err( "Server",response.data );
                 }
             }).catch((error)=>{
-                self.setLoading(false);
+                if (self.ajaxActivationCount>0) {
+                    self.ajaxActivationCount -= 1;
+                } 
                 err( "Network",error );
             });
         }
